214f97661c2d2acc94b90245e8f643f2f4e3321b
csaavedra <gabyc_gsm@yahoo.es>
2025-07-14 17:07:45 -0500

    botón cancelar

--- src/app/home-component/home-component.ts
+++ src/app/home-component/home-component.ts
@@ -65,4 +65,7 @@ export class HomeComponent implements OnInit {
     this.usuarioAEliminar = null;
   }
 
+  handleCancel() {
+  this.usuarioSeleccionado = {id: 0, name: '', address: '' , phone: ''};
+  }
 }
